aa19f99487acfdd525f9fc28480760d9a63aa2d6
Mohammad Jabir <mjabir94@proton.me>
2026-03-05 14:30:19 -0700

    challenge complete.

--- OperandCity/Program.cs
+++ OperandCity/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        BlockCoordinate first = new (10, 10);
+        BlockOffset oFive = new(5, 5);
+
+        Console.WriteLine(first + oFive);
+        Console.WriteLine(first + Direction.East);
+        Console.WriteLine(first[0] + ", " + first[1]);
+        
+        BlockOffset north = Direction.North;
+        Console.WriteLine(first + north);
     }
 }
+
+public record BlockCoordinate(int Row, int Column)
+{
+    public static BlockCoordinate operator +(BlockCoordinate bc, BlockOffset bo) =>
+        new BlockCoordinate(bc.Row + bo.RowOffset, bc.Column + bo.ColumnOffset);
+
+    public static BlockCoordinate operator +(BlockCoordinate bc, Direction d)
+    {
+        BlockCoordinate bc2 = d switch
+        {
+            Direction.North => new BlockCoordinate(bc.Row - 1, bc.Column),
+            Direction.West => new BlockCoordinate(bc.Row, bc.Column - 1),
+            Direction.South => new BlockCoordinate(bc.Row + 1, bc.Column),
+            Direction.East => new BlockCoordinate(bc.Row, bc.Column + 1),
+            _ => new BlockCoordinate(bc.Row, bc.Column)
+        };
+        
+        return bc2;
+    }
+    
+    public int this[int index]
+    {
+        get
+        {
+            if (index == 0) return Row;
+            if (index == 1) return Column;
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
+
+public record BlockOffset(int RowOffset, int ColumnOffset)
+{
+    public static implicit operator BlockOffset(Direction d)
+    {
+        BlockOffset bo = d switch
+        {
+            Direction.North => new BlockOffset(-1, 0),
+            Direction.West => new BlockOffset(0, -1),
+            Direction.South => new BlockOffset(1, 0),
+            Direction.East => new BlockOffset(0, 1),
+            _ => new BlockOffset(0,0)
+        };
+
+        return bo;
+    }
+}
+public enum Direction { North, East, South, West }
